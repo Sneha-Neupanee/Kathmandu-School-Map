1f45000eecf989a7b0a70dfc5ccc00ee70dce039
Sneha-Neupanee <neupanesneha648@gmail.com>
2026-04-19 15:03:49 +0545

    updated intro

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import Dashboard from './Dashboard.jsx'
 import "maplibre-gl/dist/maplibre-gl.css";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </StrictMode>,
 )
